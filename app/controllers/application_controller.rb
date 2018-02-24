8e5655a7faad59603c9e2e6601278515c0f110ac
Eric Rouach <erouach@hotmail.fr>
2018-02-24 14:12:49 +0100

    bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params.fetch(:locale, extract_locale_from_accept_language_header).to_sym
+    if extract_locale_from_accept_language_header == :fr || :en
+      I18n.locale = params.fetch(:locale, extract_locale_from_accept_language_header).to_sym
+    else
+      I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
+    end
   end
   def default_url_options
     { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
