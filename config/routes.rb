9d22db6fef319ce86ec7cd61c8e6f17247bfaa6f
Eric Rouach <erouach@hotmail.fr>
2018-04-13 15:51:51 +0200

    deutsch language

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope '(:locale)', locale: /fr|en/ do
+  scope '(:locale)', locale: /fr|en|de/ do
     root to: 'pages#home'
   end
 end
